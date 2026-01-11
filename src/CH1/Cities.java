42bd2758aaccdf1ef3e7827095e89b0d0680b787
yogi99k <yogikoteru99@gmail.com>
2026-01-11 13:59:58 -0500

    1st Commit Cities.java

---
+++ src/CH1/Cities.java
@@ -0,0 +1,37 @@
+/***
+ * Excerpted from "Functional Programming in Java, Second Edition",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit https://pragprog.com/titles/vsjava2e for more book information.
+ ***/
+package CH1;
+import java.util.List;
+import java.util.Arrays;
+
+public class Cities {
+
+    public static void findChicagoImperative(final List<String> cities) {
+        boolean found = false;
+        for(String city : cities) {
+            if(city.equals("Chicago")) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println("Found chicago?:" + found);
+    }
+
+    public static void findChicagoDeclarative(final List<String> cities) {
+        System.out.println("Found chicago?:" + cities.contains("Chicago"));
+    }
+
+    public static void main(final String[] args) {
+        List<String> cities = Arrays.asList("Albany", "Boulder", "Chicago", "Denver", "Eugene");
+
+        findChicagoImperative(cities);
+        findChicagoDeclarative(cities);
+    }
+}
