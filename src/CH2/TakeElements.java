b0ea00c973fab7f9007bfc941cea0dad60b036ab
yogi99k <yogikoteru99@gmail.com>
2026-02-09 21:57:01 -0500

    TakeElements(Terminating Iterations)- CH2

--- src/CH2/TakeElements.java
+++ src/CH2/TakeElements.java
@@ -3,28 +3,28 @@ package CH2;
 import java.util.Arrays;
 import java.util.List;
 
-public class SkipElements {
+public class TakeElements {
     final List<String> friends =
-            Arrays.asList( "Brian" , "Nate" , "Neal" , "Raju" , "Sara" , "Scott" );
-    public String usingSkip(){
+            Arrays.asList("Brian" , "Nate" , "Neal" , "Raju" , "Sara" , "Scott" );
+    public String usingLimit(){
         friends.stream()
-                .skip(4)
+                .limit(3)
                 .map(String::toUpperCase)
                 .forEach(System.out::println);
         return "";
     }
-    public String usingdropWhile(){
+    public String usingtakeWhile(){
         friends.stream()
-                .dropWhile(names->names.length()>4)
+                .takeWhile(n->n.length()>4)
                 .map(String::toUpperCase)
                 .forEach(System.out::println);
         return "";
     }
 
     public static void main(String[] args) {
-        SkipElements skipElements = new SkipElements();
-        skipElements.usingSkip();
+        TakeElements takeElements = new TakeElements();
+        takeElements.usingLimit();
         System.out.println("------------------");
-        skipElements.usingdropWhile();
+        takeElements.usingtakeWhile();
     }
 }
