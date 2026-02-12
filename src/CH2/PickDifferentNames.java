6549ce1ccac17087593cb79f7aed9e808863072f
yogi99k <yogikoteru99@gmail.com>
2026-02-11 22:29:18 -0500

    PickDifferentNames 2/2 - CH2

--- src/CH2/PickDifferentNames.java
+++ src/CH2/PickDifferentNames.java
@@ -1,7 +1,9 @@
 package CH2;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class PickDifferentNames {
@@ -11,6 +13,10 @@ public class PickDifferentNames {
             Arrays.asList( "Brian" , "Jackie" , "John" , "Mike" );
     static final List<String> comrades =
             Arrays.asList( "Kate" , "Ken" , "Nick" , "Paula" , "Zach" );
+
+    public static Predicate<String> checkIfStartsWith(final String letter){
+        return names->names.startsWith(letter);
+    }
 
     static final Predicate<String> startsWith= names->names.startsWith("N");
     static final Predicate<String> startsWithN = names->names.startsWith("B");
@@ -31,5 +37,48 @@ public class PickDifferentNames {
         System.out.println(countFriendsStartN);
         System.out.println(countEditorsStartB);
         System.out.println(countComradesStartN);
+        System.out.println("--------------checkIfStartsWith---------------");
+        long countFriendsStartN2 = friends.stream()
+                .filter(checkIfStartsWith("N"))
+                .count();
+        long countEditorsStartB2 = editors.stream()
+                .filter(checkIfStartsWith("B"))
+                .count();
+        System.out.println(countFriendsStartN2);
+        System.out.println(countEditorsStartB2);
+
+        System.out.println("--------------startsWith---------------");
+
+        final Function<String, Predicate<String>> startsWithLetter =
+                (String letter) -> (String name) -> name.startsWith(letter);
+
+        final long countFriendsStartN3 =
+                friends.stream()
+                        .filter(startsWithLetter.apply("N"))
+                        .count();
+
+        final long countFriendsStartB3 =
+                friends.stream()
+                        .filter(startsWithLetter.apply("B"))
+                        .count();
+
+        System.out.println(countFriendsStartN3);
+        System.out.println(countFriendsStartB3);
+
+        System.out.println("--------------startsWith-Refactor--------------");
+
+        final Function<String , Predicate<String>> stringPredicateFunction =
+                letter -> name -> name.startsWith(letter);
+        final long countFriendsStartN4 =
+                friends.stream()
+                        .filter(stringPredicateFunction.apply("N"))
+                        .count();
+        final long countFriendsStartB4 =
+                friends.stream()
+                        .filter(stringPredicateFunction.apply("B"))
+                        .count();
+        System.out.println(countFriendsStartN4);
+        System.out.println(countFriendsStartB4);
+
     }
 }
