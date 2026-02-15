e7928d36361f2f6855f19e41eb39d41e17b48ffe
yogi99k <yogikoteru99@gmail.com>
2026-02-14 19:48:09 -0500

    PickAnElement(Optional) 1/n - CH2

--- src/CH2/PickAnElement.java
+++ src/CH2/PickAnElement.java
@@ -1,6 +1,7 @@
 package CH2;
 
 import java.util.List;
+import java.util.Optional;
 
 public class PickAnElement {
     public static void pickName (final List<String> names, final String startingLetter) {
@@ -19,10 +20,29 @@ public class PickAnElement {
         }
     }
 
+    /**
+     * The Optional class is useful whenever the result may be absent. It protects us from getting a
+     * NullPointerException
+     * @param names
+     */
+    public static void pickName2 (
+            final List<String> names, final String startingLetter) {
+        final Optional<String> foundName =
+                names.stream()
+                        .filter(name ->name.startsWith(startingLetter))
+                        .findFirst();
+        System.out.println(String.format( "A name starting with %s: %s" ,
+                startingLetter, foundName.orElse( "No name found" )));
+    }
+
     public static void main(String[] args) {
         final List<String> friends =
                 List.of( "Brian" , "Nate" , "Neal" , "Raju" , "Sara" , "Scott" );
         pickName(friends, "N");
         pickName(friends, "B");
+        pickName(friends, "X");
+        System.out.println("--------------pickName2---------------");
+        pickName2(friends, "N");
+        pickName2(friends, "B");
     }
 }
